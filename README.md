b745ba9c67d31436325d1550fc2813eae439f250
Sergio Vilar <sergiovilar.r@gmail.com>
2015-08-01 01:25:58 -0300

    Implemented template rendering

--- README.md
+++ README.md
@@ -22,3 +22,27 @@ Route::get('user/:id', function($user_id){
   // Do something with $user_id
 });
 ```
+
+## Templates
+
+Ampersand uses [Twig](http://twig.sensiolabs.org/) to render templates.
+
+You can set `views` folder and `cache` property to Twig via Ampersand:
+
+```php
+<?php
+
+$Ampersand = new Ampersand();
+$Ampersand->set('cache', false);
+```
+
+And render templates using routes:
+
+```php
+<?php
+Route::get('hello/:name', function($name){
+  render('hello', ['name' => $name]); // will render views/hello.html
+})
+```
+
+You can also use layouts and template blocks, read more in [Twig documentation](http://twig.sensiolabs.org/documentation).
--- main.php
+++ main.php
@@ -1,3 +1,45 @@
 <?php
 
-require "vendor/autoload.php";
+class Ampersand {
+
+  private $twig;
+
+  private $config = [
+    'views' => 'views',
+    'cache' => 'cache',
+  ];
+
+  public function __construct(){
+    Twig_Autoloader::register();
+    $this->twigSetup();
+  }
+
+  public function set($key, $value) {
+    $this->config[$key] = $value;
+    $this->twigSetup();
+  }
+
+  public function get($key) {
+    return $this->config[$key];
+  }
+
+  private function twigSetup(){
+    $cache = $this->get('cache') ? __DIR__.'/../'.$this->config['cache'].'/' : false;
+    $loader = new Twig_Loader_Filesystem(__DIR__.'/../'.$this->config['views'].'/');
+    $this->twig = new Twig_Environment($loader, array(
+        'cache' => $cache
+    ));
+  }
+
+  public function render($template, $data = []){
+    echo $this->twig->render(str_replace('.', '/', $template).'.html', $data);
+  }
+
+}
+
+$Ampersand = new Ampersand();
+$Ampersand->set('cache', false);
+function render($template, $data = []){
+  global $Ampersand;
+  $Ampersand->render($template, $data);
+}
