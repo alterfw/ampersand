770efa22eb1e721cbf475d539105ac3cb8adfdf3
Sergio Vilar <sergiovilar.r@gmail.com>
2015-08-06 00:05:26 -0300

    Refactor in template rendering

--- src/http/Response.php
+++ src/http/Response.php
@@ -5,17 +5,11 @@ namespace Ampersand\Http;
 class Response extends \Slim\Http\Response {
 
   public function render($template, $data = []) {
-    $this->write(\Render::template($template, $data, false), true);
-  }
-
-  private function getTemplate($template){
-    ob_start();
-    require(get_query_template($template));
-    return ob_get_clean();
+    $this->write(\Render::view($template, $data), true);
   }
 
   public function template($template){
-    $this->write($this->getTemplate($template));
+    $this->write(\Render::template($template), true);
   }
 
   public function toJSON($data){
