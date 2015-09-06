efdb81b513e0f957d5d35c2da9ab7a1d7ee40f40
Sergio Vilar <sergiovilar.r@gmail.com>
2015-09-06 00:45:56 -0300

    Improved environment detection

--- src/router.php
+++ src/router.php
@@ -237,6 +237,8 @@ class Route {
   }
 
   private function getEnv() {
+    $by_env = getenv("AMPERSAND_ENV");
+    if(!empty($by_env) && !defined('AMPERSAND_ENV')) define('AMPERSAND_ENV', $by_env);
     return defined('AMPERSAND_ENV') ? AMPERSAND_ENV : 'WP';
   }
 
